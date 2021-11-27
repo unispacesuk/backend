bcd16d00561c5976ac315d4ea7957ae4a6847be1
Ricardo Rocha <me@rrocha.uk>
2021-11-27 14:16:10 +0000

    * more sql updates;

--- Database/setup/DropAll.sql
+++ Database/setup/DropAll.sql
@@ -3,6 +3,7 @@
  Use this for tests only!
  */
 
+DROP TABLE questions;
 DROP TABLE user_roles;
 DROP TABLE users;
 DROP TABLE roles;
--- Database/setup/MainSchema.sql
+++ Database/setup/MainSchema.sql
@@ -13,7 +13,7 @@
 CREATE SEQUENCE USERS_ID_AI;
 CREATE TABLE users (
     _id SMALLINT NOT NULL DEFAULT NEXTVAL('USERS_ID_AI'),
-    --username VARCHAR (100) NOT NULL UNIQUE,
+    username VARCHAR (100) NOT NULL UNIQUE,
     --first_name varchar (100) NOT NULL,
     --last_name varchar (100) NOT NULL,
     email varchar (100) NOT NULL UNIQUE,
@@ -21,6 +21,7 @@ CREATE TABLE users (
     PRIMARY KEY (_id)
 );
 ALTER SEQUENCE USERS_ID_AI OWNED BY users._id;
+-- INSERT INTO users (username, email, not_username) VALUES ('admin', 'admin@email.com', 'admin');
 
 -- roles table
 CREATE SEQUENCE ROLES_ID_AI;
@@ -37,3 +38,17 @@ CREATE TABLE user_roles (
     role_id SMALLINT NOT NULL REFERENCES roles (_id),
     PRIMARY KEY (user_id, role_id)
 );
+
+-- questions table
+CREATE TABLE questions (
+    _id UUID NOT NULL UNIQUE,
+    user_id SMALLINT REFERENCES users (_id),
+    title VARCHAR (255) NOT NULL,
+    content TEXT NOT NULL,
+    answers JSONB DEFAULT '{}',
+    PRIMARY KEY (_id)
+);
+-- INSERT INTO questions (_id, user_id, title, content) VALUES (gen_random_uuid(), 1, 'The first question', 'This will be the question content here. Text should allow for long texts....')
+-- UPDATE questions SET answers = '{"1":"first answer"}' WHERE _id='ff3cc520-47c6-4048-901b-05a9b11ca760';
+
+-- boards table
