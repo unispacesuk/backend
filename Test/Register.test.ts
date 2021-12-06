0655e18cbf2e5e0efab29623a33a730f236cd98d
Ricardo Rocha <me@rrocha.uk>
2021-12-06 23:13:49 +0000

    * tests for register;

--- Test/Register.test.ts
+++ Test/Register.test.ts
@@ -1,25 +1,60 @@
 // @ts-ignore
 import user from './Data/user.json';
-import {Connection} from "../App/Config";
-import {Client} from "pg";
+import {Config} from "../App/Config";
+import {PoolClient, Pool} from "pg";
 
 describe('Register a user', () => {
 
-  let client: Client;
+  const mock = user.user;
+  let pgPool: Pool;
+  let client: PoolClient;
+  const config = new Config();
 
-  beforeAll(() => {
-    client = Connection.client;
+  beforeAll(async () => {
+    pgPool = new Pool({
+      connectionString: config.dbUrl
+    });
+    client = await pgPool.connect();
+    await client.query('BEGIN');
   });
 
-  test('client connection', () => {
-    expect(client).toBeInstanceOf(Client);
+  afterAll(async () => {
+    await client.query('ROLLBACK');
   });
 
-  test.todo('can create user', () => {
-
+  it('Can create user.', async () => {
+    try {
+      const {rows} = await client.query(`INSERT INTO users (username, email, not_username)
+                                         VALUES ('${mock.username}', '${mock.email}',
+                                                 '${mock.not_username}')
+                                         RETURNING *`);
+      expect(rows.length).toBeGreaterThan(0);
+    } catch (e) {
+      throw e;
+    }
   });
 
-  test.todo('can retrieve user', () => {
+  it('Can retrieve user by username.', async () => {
+    try {
+      const {rows} = await client.query(`SELECT *
+                                         FROM users
+                                         WHERE username = '${mock.username}'
+                                           AND not_username = '${mock.not_username}'`);
+      expect(rows.length).toBe(1);
+    } catch (e) {
+      throw e;
+    }
+  });
 
+  it('Can retrieve user by email.', async () => {
+    try {
+      const {rows} = await client.query(`SELECT *
+                                         FROM users
+                                         WHERE email = '${mock.email}'
+                                           AND not_username = '${mock.not_username}'`);
+      expect(rows.length).toBe(1);
+    } catch (e) {
+      throw e;
+    }
   });
 });
