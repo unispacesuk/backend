3f46117c2bdf66478f90f0462bc6dc812069d230
Ricardo Rocha <me@rrocha.uk>
2021-12-23 00:46:47 +0000

    * paths resolver for jest testing;

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,18 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: [
+    '<rootDir>',
+    './App/'
+  ],
+  moduleDirectories: [
+    'node_modules'
+  ],
+  moduleNameMapper: {
+    '@Requests': '<rootDir>/App/Core/Routing',
+    '@Decorators': '<rootDir>/App/Core/Decorators',
+    '@Services/(.*)': '<rootDir>/App/Services/$1',
+    '@Models': '<rootDir>/App/Models',
+    '@Interfaces': '<rootDir>/App/Interfaces'
+  }
 };
