dd04be4d9ee8d882aa0feb959f70c5681887494e
Ricardo Rocha <me@rrocha.uk>
2022-01-12 17:38:29 +0000

    Controller global middlewares
    * new logic to allow for global controller middlewares;
    * new IController interface (registers the path and a middlewares
      array);

--- App/Core/Decorators/ControllerDecorator.ts
+++ App/Core/Decorators/ControllerDecorator.ts
@@ -1,13 +1,21 @@
 import 'reflect-metadata';
+import { IController } from '../../Interfaces/IController';
 
 /**
  * This will allow us to register a controller route (api.unispaces.uk/controller/....)
  * Then we don't need to create groups of routes and just need to import the controller in Controllers.ts
  * @param path
+ * @param middlewares
  * @constructor
  */
-export function Controller(path: string) {
-  return (target: any) => {
-    Reflect.defineMetadata('controller', path, target);
+export function Controller(path: string, middlewares?: ((key: string) => void)[] | undefined) {
+  return (target: object) => {
+    const controller: IController = {
+      path: path,
+      middlewares: middlewares,
+    };
+    Reflect.defineMetadata('controller', controller, target);
+    // Reflect.defineMetadata('controllerMiddlewares', middlewares, target);
+    // Reflect.defineMetadata('controller', path, target);
   };
 }
--- App/Interfaces/IController.ts
+++ App/Interfaces/IController.ts
@@ -1,4 +1,4 @@
 export interface IController {
   path: string;
-  target: () => void;
+  middlewares?: ((key: string) => void)[] | undefined;
 }
