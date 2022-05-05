990785cbea0fe68a5a9683c166ef3c10c9983a35
Ricardo Rocha <me@rrocha.uk>
2022-05-05 19:37:27 +0100

    * cascade on drop all;

--- Database/setup/DropAll.sql
+++ Database/setup/DropAll.sql
@@ -3,23 +3,23 @@
  Use this for tests only!
  */
 
-DROP TABLE events;
-DROP TABLE resource_files;
-DROP TABLE private_messages;
-DROP TABLE chat_room_messages;
-DROP TABLE chat_rooms;
-DROP TABLE blog_votes;
-DROP TABLE blog_vote_types;
-DROP TABLE blog_comments;
-DROP TABLE blog_posts;
-DROP TABLE board_threads_stars;
-DROP TABLE board_thread_replies;
-DROP TABLE board_threads;
-DROP TABLE board_boards;
-DROP TABLE board_categories;
-DROP TABLE questions_votes;
-DROP TABLE answers;
-DROP TABLE questions;
-DROP TABLE user_roles;
-DROP TABLE users;
-DROP TABLE roles;
+DROP TABLE events CASCADE;
+DROP TABLE resource_files CASCADE;
+DROP TABLE private_messages CASCADE;
+DROP TABLE chat_room_messages CASCADE;
+DROP TABLE chat_rooms CASCADE;
+DROP TABLE blog_votes CASCADE;
+DROP TABLE blog_vote_types CASCADE;
+DROP TABLE blog_comments CASCADE;
+DROP TABLE blog_posts CASCADE;
+DROP TABLE board_threads_stars CASCADE;
+DROP TABLE board_thread_replies CASCADE;
+DROP TABLE board_threads CASCADE;
+DROP TABLE board_boards CASCADE;
+DROP TABLE board_categories CASCADE;
+DROP TABLE questions_votes CASCADE;
+DROP TABLE answers CASCADE;
+DROP TABLE questions CASCADE;
+DROP TABLE user_roles CASCADE;
+DROP TABLE users CASCADE;
+DROP TABLE roles CASCADE;
