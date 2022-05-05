a3debbb9b90aceac6ad105bae0bd8ecc8c56e648
Ricardo Rocha <me@rrocha.uk>
2022-05-05 21:26:25 +0100

    * Fixed user invitation to private rooms;

--- App/Services/RoomsService/RoomsService.ts
+++ App/Services/RoomsService/RoomsService.ts
@@ -164,7 +164,7 @@ export class RoomsService {
   static async isUserInRoom(roomId: string, invitee: number) {
     const result = await this.client.query('SELECT users FROM chat_rooms WHERE _id = $1', [roomId]);
 
-    if (!result.rows.length) return Promise.resolve(null);
+    if (!result.rows[0].users) return Promise.resolve(null);
 
     return Promise.resolve(result.rows[0].users.includes(invitee));
   }
